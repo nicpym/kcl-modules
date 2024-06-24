b19101e57080b04b0b87ce64b7a4b08a4698250b
Nicholas Pym <nicholas.pym@nn.nl>
2024-06-24 23:08:01 +0200

    initial commit

---
+++ crossplane-provider-aws-s3/generate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+crds=$(kubectl get crds -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep 's3.aws.upbound.io')
+
+rm -rf ./crds.yaml
+
+touch ./crds.yaml
+
+for CRD in $crds
+do
+    echo "---" >> ./crds.yaml
+    kubectl get crd $CRD -o yaml >> ./crds.yaml
+done
+
+rm -rf temp
+
+kcl import -m crd -p temp ./crds.yaml
+
+# Get all files in temp dir that have v1beta1 in name
+files=$(find ./temp -type f -name "*v1beta1*" | sed 's|.*/||')
+
+rm -rf v1beta1
+mkdir -p v1beta1
+
+# Move the files to v1beta1 folder
+for file in $files; do
+    mv ./temp/${file} ./v1beta1/${file}
+done
+
+rm -rf temp
+rm -rf ./crds.yaml
+
+kcl fmt ./v1beta1
