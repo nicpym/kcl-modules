b6584d0afca3ec14a68e7423316830b7bb53e68c
Nicholas Pym <nicholas.pym@nn.nl>
2024-06-24 23:48:58 +0200

    added more providers

---
+++ crossplane-provider-aws-ec2/generate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+crds=$(kubectl get crds -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep 'ec2.aws.upbound.io')
+
+rm -rf ./crds.yaml
+
+touch ./crds.yaml
+
+for CRD in $crds
+do
+    echo "---" >> ./crds.yaml
+    kubectl get crd $CRD -o yaml >> ./crds.yaml
+done
+
+rm -rf temp
+
+kcl import -m crd -p temp ./crds.yaml
+
+# Get all files in temp dir that have v1beta1 in name
+filesV1Beta1=$(find ./temp -type f -name "*v1beta1*" | sed 's|.*/||')
+filesV1Beta2=$(find ./temp -type f -name "*v1beta2*" | sed 's|.*/||')
+
+rm -rf v1beta1
+mkdir -p v1beta1
+rm -rf v1beta2
+mkdir -p v1beta2
+
+for file in $filesV1Beta1; do
+    mv ./temp/${file} ./v1beta1/${file}
+done
+
+for file in $filesV1Beta2; do
+    mv ./temp/${file} ./v1beta2/${file}
+done
+
+rm -rf temp
+rm -rf ./crds.yaml
+
+kcl fmt ./v1beta1
+kcl fmt ./v1beta2
---
+++ crossplane-provider-aws-opensearch/kcl.mod
@@ -0,0 +1,6 @@
+[package]
+name = "crossplane-provider-aws-opensearch"
+version = "1.1.0"
+
+[dependencies]
+k8s = { oci = "oci://ghcr.io/kcl-lang/k8s", tag = "1.28" }
